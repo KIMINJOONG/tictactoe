e500ea023dd33ba3afcbfc565d6852378ca12699
KIMINJOONG <ijboym216@gmail.com>
2019-01-01 21:05:16 +0900

    Square.js에 state생성 onClick도 변경

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class Square extends Component {
+    state = {
+        value: null
+    }
     render() {
         return(
-            <button className="square">
-                { this.props.value }  {/* props value를 렌더링 */}
+            <button 
+                className="square"
+                onClick= {() => { this.setState({ value: 'X'}) }}
+                >
+                { this.state.value }  {/* props value를 렌더링 */}
             </button>
         );
     }
